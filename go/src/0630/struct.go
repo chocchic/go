5ae913987ed0a2983acf60a1c0bf053a3a5b5f4b
chocchic <dntksemfdj473@gmail.com>
2022-06-30 15:20:40 +0900

    0630 5

--- go/src/0630/struct.go
+++ go/src/0630/struct.go
@@ -26,6 +26,35 @@ func NewMyStruct(intField int, stringField string, sliceField []int) *myStruct {
 	return imsi
 }
 
+type Rect struct {
+	width  int
+	height int
+}
+
+// Rect 구조체의 필드의 값을 가져오는 메서드와 설정하는 메서드
+func (rect *Rect) getWidth() int {
+	return rect.width
+}
+func (rect *Rect) getHeight() int {
+	return rect.height
+}
+func (rect *Rect) setWidth(width int) {
+	rect.width = width
+}
+func (rect *Rect) setHeight(height int) {
+	rect.height = height
+}
+
+// 메서드를 만들 때 *을 생략하고 리시버를 만들면 리시버는 호출하는 리시버의 데이터가 새로 만들어져서 대입됩니다.
+func (rect Rect) copyHeight(height int) {
+	rect.height = height
+}
+
+// 리시버가 메서드 안에서 사용이 안될 때는 _로 이름을 만드는 것이 가능
+func (_ *Rect) display() {
+	Println("나는 길이와 너비를 가지고 있습니다.")
+}
+
 func main() {
 	// 구조체 초기화
 	var s1 = myStruct{
@@ -54,4 +83,17 @@ func main() {
 
 	m := NewMyStruct(4, "zlzlzl", []int{102, 103, 104, 105})
 	Println(m)
+
+	// Rect 구조체와 연결된 메서드 사용
+	rect := Rect{}
+	rect.setHeight(10)
+	rect.setWidth(20)
+	Println(rect.getWidth(), rect.getHeight())
+
+	// copyHeight 메서드는 height를 매개변수의 값으로 변경하도록 되어있지만
+	// 리시버가 포인터 형태가 아니기 때문에 호출하는 리시버의 데이터를 변경할 수 없습니다.
+	rect.copyHeight(30)
+	Println(rect.getHeight(), rect.getHeight())
+
+	rect.display()
 }
